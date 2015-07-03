3dedd71b783ff2a64c69a53f914ae54a118c9e60
Nathan Miniovich <nathan.miniovich@rutgers.edu>
2015-07-03 16:53:22 +0900

    Compiles for all 32 bit archs
    Had to remove C flags since they were causing a conflict with premade
    android architecture packaged when configure was run

--- build_openssl.sh
+++ build_openssl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+#It is recommended to isntall this patch: http://www.linuxfromscratch.org/blfs/view/svn/postlfs/openssl.html
+
 export NDK= (add ndk path here)
 
-cd openssl-1.0.1o && mkdir android-archs
-cd ..
+mkdir openssl-android-archs
 
 function buildLibs(){
 
@@ -17,13 +18,9 @@ function buildLibs(){
          export CC="$3-gcc --sysroot $SYSROOT"
 
          # Configure
-         cd openssl-1.0.1o && mkdir android-archs/build_$1
-         ./Configure $5 no-shared no-thread -fPIC -DOPENSSL_PIC --prefix=$(pwd)/android-archs/build_$1 \
-         CFLAGS="$4"
-
-         push include
-         make
-         popd
+         mkdir -p openssl-android-archs/build_$1 && cd openssl-1.0.2c
+         ./Configure $5 no-shared no-thread -fPIC -DOPENSSL_PIC --prefix=$(pwd)/../openssl-android-archs/build_$1 
+#        CFLAGS="$4" "REMOVED" C-flags cause a conflict with with android architecture packages when runningin configure
 
          # Build and install
          make clean
@@ -32,10 +29,12 @@ function buildLibs(){
          cd ..
 }
 
-#buildLibs androideabiv7a arm-linux-androideabi-4.9 arm-linux-androideabi -march=armv7-a android-armv7
-#buildLibs androideabi arm-linux-androideabi-4.9 arm-linux-androideabi "-minline-thumb1-jumptable -march=armv5te" android
-#buildLibs mips mipsel-linux-android-4.9 mipsel-linux-android -g android-mips
+buildLibs androideabiv7a arm-linux-androideabi-4.9 arm-linux-androideabi -march=armv7-a android-armv7
+buildLibs androideabi arm-linux-androideabi-4.9 arm-linux-androideabi "-minline-thumb1-jumptable -march=armv5te" android
+buildLibs mips mipsel-linux-android-4.9 mipsel-linux-android -g android-mips
 buildLibs x86 x86-4.9 i686-linux-android -g android-x86
+
+#64 bit builds not working yet
 #buildLibs x86_64 x86_64-4.9 x86_64-linux-android -g android-x86_64
 #buildLibs aarch64 aarch64-linux-android-4.9 aarch64-linux-android -march=armv8-a android-armv8
 
